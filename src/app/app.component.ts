7564b5474e7b6fd84c8294ae903c4e27d7a1e9da
Javier Arocena <arocena.javier@gmail.com>
2020-10-09 13:21:56 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-graphql';
+
+  contries: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    const conceptName = 'countries';
+    const inputsName = ['code','name','capital'];
+    this.contries = this.apollo
+      .watchQuery({
+        query: gql`
+        {
+          ${conceptName} {
+            ${inputsName.join(',')}
+          }
+        }
+      `,
+      })
+      .valueChanges.pipe(map((result) => (result.data as any)[conceptName]));
+  }
+
 }
